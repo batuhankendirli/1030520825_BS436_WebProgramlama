be798d0a44cdc9c112c28811ad190daf2c76c43f
batuhankendirli <batuhankndrl@gmail.com>
2021-03-31 21:32:53 +0300

    Odev04_Son

---
+++ odev04/tests/Game-test.jsx
@@ -0,0 +1,18 @@
+const React = require("react");
+const { mount } = require("enzyme");
+const { Game } = require("../src/Game");
+
+//Kart Tıklama Durumu Testi
+test("click", () => {});
+
+//Kaybetme Durumu Testi
+
+test("lose", () => {});
+
+//Kazanma Durumu Testi
+
+test("win", () => {});
+//Yeni Oyun Durumu Testi
+
+test("yeniOyun", () => {});
+// Geçen haftaki ödevi yapamadağımdan ve bu hafta da geçen haftaya bağlı olduğundan yapamadım.
